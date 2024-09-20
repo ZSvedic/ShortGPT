425a891bf5437d5c2bd67db4c3e3b910f523a91c
ZSvedic <zsvedic@gmail.com>
2024-09-20 17:34:07 +0200

    Changed error CLI instructions, added summary, raise exception in invalid response.

--- 2_brevity_benchmark/benchmark_cli.py
+++ 2_brevity_benchmark/benchmark_cli.py
@@ -3,34 +3,31 @@ import time
 import torch
 import datasets
 from datasets.utils import disable_progress_bar
+import pandas as pd
 import utils.llm_utils as llm
 
 manual = '''CLI py app that inputs a JSONL file where each row has 2+ answers to a question and outputs a JSONL file that has the shortest answer to the question that still contains the non-trivial answer. App is called as:
 
-python benchmark_cli.py in_jsonl out_jsonl evaluator continue/restart
+python benchmark_cli.py in_jsonl out_jsonl model continue/restart
 
-Where the evaluator is either "length" or the name of the HuggingFace LLM model.
+Where the model is the name of the HuggingFace LLM model.
 For example:
-
-python benchmark_cli.py in-short-answers.jsonl out-best.jsonl length restart
-
-will compare based on length, while:
 
 python benchmark_cli.py in-short-answers.jsonl out-best.jsonl meta-llama/Meta-Llama-3.1-8B-Instruct continue
 
 will use Llama-3.1-8B to evaluate the best answers.
 
-Input JSONL fields are ID, Question, Answer-X, Answer-Y, etc. Output JSONL fields are ID, Question, Name-best, and Answer-best. Field ID contains integers starting from 101. Fields Question, Answer-X, Answer-Y, Answer are texts. The name of the answer field must begin with "Answer-"; the part after that is the descriptive name. The name-best field contains that descriptive name.
+Input JSONL fields are id, question, answer-x, answer-y, etc. Output JSONL fields are id, question, name-best, and answer-best. Field id contains integers starting from 101. Fields question, answer-x, answer-y,  are texts. The name of the answer field must begin with "answer-"; the part after that is the descriptive name. The name-best field contains that descriptive name.
 
 For example, for input JSONL:
 
-{"ID": "101", "Question": "How much is 2+3?", "Answer-ChatGPT": "Expression 2+3 is equal to 5.", "Answer-Short": "5"}
-{"ID": "102", "Question": "What is the color of the sky?", "Answer-ChatGPT": "The sky is blue", "Answer-Short": "sky"}
+{"id": "101", "question": "How much is 2+3?", "answer-chatgpt": "Expression 2+3 is equal to 5.", "answer-Short": "5"}
+{"id": "102", "question": "What is the color of the sky?", "answer-chatgpt": "The sky is blue", "answer-Short": "sky"}
 
 Output JSONL is:
 
-{"ID": "101", "Question": "How much is 2+3?", "Name-best": "Short", "Answer-best": "5"}
-{"ID": "102", "Question": "What is the color of the sky?", "Name-best": "ChatGPT", "Answer-best": "The sky is blue"}
+{"id": "101", "question": "How much is 2+3?", "Name-best": "Short", "answer-best": "5"}
+{"id": "102", "question": "What is the color of the sky?", "Name-best": "ChatGPT", "answer-best": "The sky is blue"}
 
 As output, the app prints errors and a summary with the number of wins, average length, and a win example for each descriptive name. For the above file, the output should be:
 
@@ -82,8 +79,10 @@ def main_logic(in_jsonl: str, out_jsonl: str, evaluator: str, mode: str) -> None
     print(f'Allocated GPU memory: {torch.cuda.memory_allocated() / (1024*1024):,.1f} MB')
 
     # Process rows in chunks and save each chunk to a JSONL file.
-    process_dataset(tokenizer, model, answer_cols, answer_names, dataset, out_jsonl,
-                    100, 5000, 10)
+    dataset = process_dataset(tokenizer, model, answer_cols, answer_names, 
+                              dataset, out_jsonl, 100, 5000, 10)
+    
+    print_summary(dataset.to_pandas())
 
 prompt_briefest_answer = ''' Given a question and multiple answers, your task is to select the briefest answer that still answers the question. Examples between --- lines:
 --- Example 1 ---
@@ -110,7 +109,7 @@ A
 --- End of examples
 Note that:
 - The briefest answer is not the shortest if it doesn't answer the question (example 2) or is empty or invalid (example 3). 
-- You always output just the ID of a question (A, B, etc.), nothing else, like spaces or explanations.
+- You always output just the id of a question (A, B, etc.), nothing else, like spaces or explanations.
 
 Given all this, what is the briefest answer to the question and answers below?
 '''
@@ -135,43 +134,74 @@ def get_options_and_prompts(ds: datasets.Dataset, answer_cols: list,
         row_prompts.append(prompt_briefest_answer + input_text)
     return rows_options, row_prompts 
 
-def get_answer_idx(answer: str) -> int:
+def get_answer_idx(row) -> int:
     ''' Get the index of the answer from the answer string. '''
-    return ord(answer.strip())-ord('A')
+    idx = ord(row['answer'].strip()[0])-ord('A')
+    max = len(row['options'])
+    if idx < 0 or idx >= max:
+        raise ValueError(f"Invalid answer index: {idx} for options: {row['options']}")
+    else:
+        return idx
 
 def process_dataset(tokenizer, model, answer_cols: list, answer_names: list,
                     ds: datasets.Dataset, out_jsonl:str,
-                    big_chunk_size: int, small_chunk_tokens: int, max_gen_tokens: int) -> None:
+                    big_chunk_size: int, small_chunk_tokens: int, 
+                    max_gen_tokens: int) -> datasets.Dataset:
     ''' Process the input dataset in chunks and save each chunk to a JSONL file. '''
 
     n_rows = len(ds)
     start_time = time.time()
     current_example = 0
     print(f"Example {current_example} of {n_rows}... ", end='')
-    
+    all_chunks = []
+
     for big_chunk in llm.fixed_chunker(ds, big_chunk_size):
-        rows_options, row_prompts = get_options_and_prompts(big_chunk, answer_cols, answer_names)
-        big_chunk = big_chunk.map(lambda _, idx: {'prompt': row_prompts[idx]}, with_indices=True)
+        rows_options, rows_prompts = get_options_and_prompts(big_chunk, answer_cols, answer_names)
+        big_chunk = big_chunk.map(lambda _, idx: {'options': rows_options[idx]}, with_indices=True)
+        big_chunk = big_chunk.map(lambda _, idx: {'prompt': rows_prompts[idx]}, with_indices=True)
         
         ids_min = []
         for small_chunk in llm.call_variable_chunks(
             big_chunk, tokenizer, model, big_chunk_size, small_chunk_tokens, max_gen_tokens):
             for row in small_chunk:
-                ids_min.append(get_answer_idx(row['answer']))
+                ids_min.append(get_answer_idx(row))
 
         big_chunk = big_chunk.map(
             lambda _, idx: {'name-best': answer_names[ids_min[idx]]}, with_indices=True)
         big_chunk = big_chunk.map(
             lambda _, idx: {'answer-best': rows_options[idx][ids_min[idx]]}, with_indices=True)
         big_chunk = big_chunk.select_columns(['id', 'question', 'name-best', 'answer-best'])
+
+        all_chunks.append(big_chunk)
         
-        with open(out_jsonl, 'ab') as f:
-            big_chunk.to_json(f, lines=True)
+        if out_jsonl:
+            with open(out_jsonl, 'ab') as f:
+                big_chunk.to_json(f, lines=True)
 
         print(f"time/sample: {(time.time()-start_time)/len(big_chunk):.2f} sec") 
         start_time = time.time()
         current_example += len(big_chunk)
-        print(f"Example {current_example} of {n_rows}... ", end='')
+        if current_example < n_rows:
+            print(f"Example {current_example} of {n_rows}... ", end='')
+
+    return datasets.concatenate_datasets(all_chunks)
+
+def print_summary(out_df: pd.DataFrame) -> None:
+    ''' Print a summary of the results. '''
+    
+    # Group by "Name-best" and perform the aggregations
+    summary = out_df.groupby("name-best").agg(
+        Wins=('name-best', 'count'),
+        Win_avg_length=('answer-best', lambda x: round(x.str.len().mean(), 1)),
+        Win_example=('answer-best', 'first'),
+    )
+    # Calculate winning percentages.
+    summary['Wins %'] = (summary['Wins'] / len(out_df)) * 100
+    # Reorder columns.
+    summary = summary[['Wins', 'Wins %', 'Win_avg_length', 'Win_example']]
+
+    print(summary)
+    print(f"All winners average length: {round(out_df['answer-best'].str.len().mean(), 1)}")
 
 if __name__ == '__main__':
     main_click()
